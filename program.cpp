2cbea6ecf3bf10fb345c1e624d297e4646124b6c
Yubesh <yubeshbhattarai@gmail.com>
2024-07-04 22:33:03 +0545

    ..

---
+++ program.cpp
@@ -0,0 +1,13 @@
+// Write a program that uses a for loop to output the numbers from 1 to 10 
+//using escape sequence.
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i;
+    for (i = 1; i <= 10; i++)
+    {
+        cout << i <<"\t";
+    }
+    return 0;
+}
